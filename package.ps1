d58f1daeba204b3ae53953bc9f0842a290d0c681
Matthew Green <Matthew.J.Green@outlook.com>
2023-07-22 16:19:59 +0100

    Updated packaging and readme

--- package.ps1
+++ package.ps1
@@ -4,6 +4,10 @@ param (
 )
 
 Set-Location "$PSScriptRoot"
+$VERSION = (Select-String -Pattern '<Version>([0-9]+\.[0-9]+\.[0-9]+)</Version>' -Path $PSScriptRoot/*/*.csproj).Matches.Groups[1]
+$modName = (Select-String -Pattern '<AssemblyName>(.*)</AssemblyName>' -Path $PSScriptRoot/*/*.csproj).Matches.Groups[1]
+
+Write-Host "Packaging $modName version $VERSION"
 
 $DistDir = "$OutputDirectory/dist"
 if ($NoArchive)
@@ -14,19 +18,35 @@ else
 {
 	$ZipWorkDir = "$DistDir/tmp"
 }
-$ZipRootDir = "$ZipWorkDir/BepInEx"
-$ZipInnerDir = "$ZipRootDir/plugins/dvDirectInput/"
-$BuildDir = "build"
-$LicenseFile = "LICENSE"
-$AssemblyFile = "$BuildDir/*.*"
 
-New-Item "$ZipInnerDir" -ItemType Directory -Force
-Copy-Item -Force -Path "$LicenseFile", "$AssemblyFile" -Destination "$ZipInnerDir"
+$ZipRootDir = "$ZipWorkDir/$modName"
+$LicenseFile = "LICENSE"
+$AssemblyFiles = "build/*.*"
+
+$null = New-Item "$ZipRootDir" -ItemType Directory -Force
+Copy-Item -Force -Path "$LicenseFile", "$AssemblyFiles" -Destination "$ZipRootDir"
+$null = New-Item "${ZipRootDir}\info.json" -Force -ItemType File -Value (@"
+{
+	"Id": "${modname}",
+	"DisplayName": "${modname}",
+	"Author": "Greeny",
+	"Version": "${VERSION}",
+	"AssemblyName": "${modname}.dll",
+	"EntryMethod": "${modname}.Main.Load",
+	"HomePage": "https://www.nexusmods.com/derailvalley/mods/687",
+	"Repository": "https://raw.githubusercontent.com/Matthew-J-Green/dvDirectInput/master/repository.json"
+}	
+"@)
 
 if (!$NoArchive)
 {
-	$VERSION = (Select-String -Pattern '<Version>([0-9]+\.[0-9]+\.[0-9]+)</Version>' -Path dvDirectInput/dvDirectInput.csproj).Matches.Groups[1]
-	$FILE_NAME = "$DistDir/dvDirectInput_v$VERSION.zip"
-	Compress-Archive -Update -CompressionLevel Fastest -Path "$ZipRootDir" -DestinationPath "$FILE_NAME"
+	$archivePath = "${DistDir}/${modname}_v$VERSION.zip"
+	Compress-Archive -Force -Path "$ZipRootDir" -DestinationPath "$archivePath"
 	Remove-Item -LiteralPath "$ZipWorkDir" -Force -Recurse
+
+	Write-Host "Archived at $archivePath"
+}
+else
+{
+	Write-Host "Copied Assemblies, License and created Info file to ${ZipRootDir}"
 }
